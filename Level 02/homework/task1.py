395248287ae1ba90c6676bd4739530c0a6c9a76a
qweqwe1qq <qweqwe@gmail.com>
2024-05-24 09:59:46 -0700

    commit

---
+++ Level 02/homework/task1.py
@@ -0,0 +1,4 @@
+first = "tamashebi cudia, adamianebi romlebic Tamashoben ar emorchilebian Shoblebs da xshirad mosdit ertmannetan compliqti, adre to gvian gaqreba es tamashebi da araferi gveqneba chveni dakargguli drois nacvlad."
+second = "davit berkacashvili/giorgi varazashili"
+print(first)
+print(second)
---
+++ Level 03/homework/task1.py
@@ -0,0 +1,1 @@
+#vuyure  vidios
