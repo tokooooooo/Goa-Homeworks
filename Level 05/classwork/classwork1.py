17e23ed23b0a850e57d780f4aa44006ffd20a05f
qweqwe1qq <qweqwe@gmail.com>
2024-05-26 01:03:45 -0700

    commit

--- Level 05/classwork/classwork1.py
+++ Level 05/classwork/classwork1.py
@@ -1,1 +1,16 @@
-print("tornike")
+name = tornikecxartishvili# type: ignore
+
+age = 10
+
+addres = nottellingyou
+
+favfood= xinkali
+
+favcoller= gold
+
+favcar=toyotasupra
+
+
+print (name,age,addres,favfood,favcoller,favcar)
+
+#naxe chemsaxels ar mawerinebs, amito to mixtebi mitxari :)
