a6d276d30c7cddbec732ebfcedc3ed96c32f8028
tokooooooo <166244554+tokooooooo@users.noreply.github.com>
2024-07-12 09:07:17 -0700

    Delete Level 02/homework directory

--- Level 02/homework/task1.py
+++
@@ -1,4 +0,0 @@
-first = "tamashebi cudia, adamianebi romlebic Tamashoben ar emorchilebian Shoblebs da xshirad mosdit ertmannetan compliqti, adre to gvian gaqreba es tamashebi da araferi gveqneba chveni dakargguli drois nacvlad."
-second = "davit berkacashvili/giorgi varazashili"
-print(first)
-print(second)
