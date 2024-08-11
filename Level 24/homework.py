679e71a881222cf3f7c74d9c3719249e4a808a63
qweqwe1qq <qweqwe@gmail.com>
2024-08-11 00:02:04 -0700

    commit

---
+++ Level 24/homework.py
@@ -0,0 +1,24 @@
+for i in range(10):
+ print( ' hello world! ' )
+ 
+for numbers in range(5, 150):
+ print(numbers)
+
+for numbers in range(5, 150, +4):
+ print(numbers)
+
+ for numbers in range(150, 5, -5):
+    print(numbers)
+
+for i in range(12):
+    print( ' goa best ' )
+    print( ' goa gigachad ' )
+
+for numbers in range(5, 150, +10):
+ print(numbers)
+
+for numbers in range(5, 304, +4):
+ print(numbers)
+
+for numbers in range(8346, 5, -5):
+    print(numbers)
--- goa oriented academy/Level 15/homework/homework.py
+++ goa oriented academy/Level 15/homework/homework.py
@@ -9,6 +9,15 @@ print (30 < 29 and 101001010101 < 1)
 print (True or True)
 print ( 364>45 and 23444<33 and 47574 > 1 and 5065 > 1234 )
 print (True and False)
+
+
+#2
+print (GO RUN 1000 STEPS YOU HAVE 10 MINS. )
+print ( did you do it) print ( True or False)
+print ( good job!)
+
+
+
 #3
 #and ze ro gaketeb true and false yoveltvis gamoitas false magram to true and true ari gamoitans trues rac shexeba true or false yoveltvis gamoitans trues  to ese ar ari:false or false mashin gamoitans false
 ========
